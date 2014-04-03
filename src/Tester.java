4603b304f9c17a36eb8dbc5c95ec94306eb9f57a
Jonathan Dimmick <xxtheamazingwebheadxx@gmail.com>
2014-04-03 16:46:01 -0700

    Modified Classes and created Tester class
    debugging needed

--- src/Tester.java
+++ src/Tester.java
@@ -1,14 +1,58 @@
-
-public class Tester extends business {
-
-	public Tester(int accnum, double rectotal) {
-		super(accnum, rectotal);
-		// TODO Auto-generated constructor stub
-	}
+public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Restaurant moes = new Restaurant(1);
+		moes.setName("Moe's");
+		Restaurant joes = new Restaurant(2);
+		joes.setName("Joe's");
+		Hotel ritz = new Hotel(3);
+		ritz.setName("Ritz");
+		Hotel notel = new Hotel(4);
+		notel.setName("Notel");
+		Convenience six12 = new Convenience(612);
+		six12.setName("six12");
+		
+		//Week1
+		joes.setReceipt("food", 4000);
+		joes.setReceipt("drink", 3500);
+		moes.setReceipt("food", 14000);
+		moes.setReceipt("drink", 9000);
+		
+		ritz.setOccupancyRate(98);
+		ritz.setReceipt("occupancy",25000);
+		notel.setOccupancyRate(93);
+		notel.setReceipt("occupancy", 5500);
+		
+		six12.setReceipt("newspaper", 2400);
+		six12.setReceipt("other", 6000);
+		
+		//Week2
+		joes.setReceipt("food", 2500);
+		joes.setReceipt("drink", 3500);
+		moes.setReceipt("food", 12500);
+		moes.setReceipt("drink", 12500);
+		
+		ritz.setOccupancyRate(88);
+		ritz.setReceipt("occupancy", 21500);
+		notel.setOccupancyRate(98);
+		notel.setReceipt("occupancy", 6575);
+		
+		six12.setReceipt("newspaper", 1700);
+		six12.setReceipt("other", 5927);
+		
+		//Display Totals
+		moes.printTotals("food");
+		moes.printTotals("drink");
+		moes.totalReceipt();
+		joes.printTotals("food");
+		joes.printTotals("drink");
+		joes.totalReceipt();
+		
+		ritz.printTotals();
+		notel.printTotals();
+		
+		six12.printTotals("newspaper");
+		six12.printTotals("other");
 	}
 
 }
--- src/business.java
+++ src/business.java
@@ -1,12 +1,12 @@
 /* Jonathan Dimmick & Steven Welch
    CSC 322 Assignment 4 */
-//test3
 
 
 abstract class business {
 	protected int accnum = 0;
 	protected double rectotal = 0;
 	protected double taxes = 0;
+	private String name;
 	
 	public business(int accnum) {
 		this.accnum = accnum;
@@ -18,59 +18,62 @@ abstract class business {
 		this.rectotal = rectotal;
 	}
 	
-	public double setReceipt(String type, double total) {
-		// calculates receipt total for drinks
-		if (type.equals("drink") == true) {
-				Restaraunt.drinktotal += (Restaraunt.drinktax * total) + total;
-			}
-		// calculates receipt total for food
-		else if (type.equals("food") == true) {
-			Restaraunt.foodtotal += total;
-		}
-		// calculates receipt total for other items
-		else if(type.equals("other") == true) {
-			this.rectotal += (Convenience.othertax * total) + total;
-		}
-		else if(type.equals("occupancy") == true) {
-			if(Hotel.occupancyrate > 90) {
-				this.rectotal += (Hotel.occupancy * total) + total;
-			}
-		}
-		
-		else {
-			this.rectotal += total;
-		}
-		
-		return rectotal;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
-class Restaraunt extends business {
+class Restaurant extends business {
 	static double drinktax = .08;
-	static double foodtotal;
-	static double drinktotal;
+	double foodtotal;
+	double drinktotal;
 
-	public Restaraunt(int accnum) {
+	public Restaurant(int accnum) {
 		super(accnum);
 	}
 	
-	public Restaraunt(int accnum, double rectotal) {
+	public Restaurant(int accnum, double rectotal) {
 		super(accnum, rectotal);
 	}
 	
 	public void totalReceipt() {
-		if(Restaraunt.foodtotal > Restaraunt.drinktotal) {
-			this.rectotal = (foodtotal + drinktotal)*.03;
+		if(this.foodtotal > this.drinktotal) {
+			this.rectotal = (this.foodtotal + this.drinktotal)*.03;
 		}
 		
 		else {
-			this.rectotal = foodtotal + drinktotal;
+			this.rectotal = this.foodtotal + this.drinktotal;
+		}
+		System.out.println(getName() + " receipt total: " + this.rectotal);
+	}
+	
+	public void setReceipt(String type, double total) {
+		if (type.equals("drink") == true) {
+			this.drinktotal += (Restaurant.drinktax * total) + total;
+		}
+	// calculates receipt total for food
+		else if (type.equals("food") == true) {
+		this.foodtotal = this.foodtotal + total;
+		}
+	}
+	
+	public void printTotals(String type) {
+		if(type.equals("food") == true) {
+			System.out.println(getName() + " food total: " + this.foodtotal);
+		}
+		else if (type.equals("drink") == true) {
+			System.out.println(getName() + " drink total: " + this.drinktotal);
 		}
 	}
 }
 
 class Hotel extends business {
 	static double occupancy = .02;
-	static int occupancyrate;
+	static double totaltax = .06;
+	int occupancyrate;
 	
 	public Hotel(int accnum) {
 		super(accnum);
@@ -81,7 +84,21 @@ class Hotel extends business {
 	}
 	
 	public void setOccupancyRate(int ocrate) {
-		Hotel.occupancyrate = ocrate;
+		this.occupancyrate = ocrate;
+	}
+	
+	public void printTotals() {
+		System.out.println(getName() + " total taxes: " + this.rectotal);
+	}
+	
+	public void setReceipt(String type, double total) {
+		if(type.equals("occupancy") == true) {
+			if(this.occupancyrate > 90) {
+				this.rectotal += (Hotel.occupancy * total) + total;
+			}
+			
+		this.rectotal += (Hotel.totaltax * this.rectotal);
+		}
 	}
 
 }
@@ -90,6 +107,8 @@ class Hotel extends business {
 class Convenience extends business {
 	static double newspapertax = 0.0;
 	static double othertax = .07;
+	double newspapertotal;
+	double othertotal;
 
 	public Convenience(int accnum) {
 		super(accnum);
@@ -98,5 +117,22 @@ class Convenience extends business {
 	public Convenience(int accnum, double rectotal) {
 		super(accnum, rectotal);
 	}
-
+	
+	public void printTotals(String type) {
+		if(type.equals("newspapertax") == true) {
+			System.out.println(getName() + " newspaper total: " + this.newspapertotal);
+		}
+		else if (type.equals("other") == true) {
+			System.out.println(getName() + " other total: " + this.othertotal);
+		}
+	}
+	
+	public void setReceipt(String type, double total) {
+		if(type.equals("other") == true) {
+			this.othertotal += (Convenience.othertax * total) + total;
+		}
+		else if (type.equals("newspaper") == true) {
+			this.newspapertotal = Convenience.newspapertax + total;
+		}
+	}
 }
